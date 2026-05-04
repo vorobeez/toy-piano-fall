438a8382ffcab98609878177ff6fdb496484fda0
Andrei Firsov <andrei.firsov@fastmail.com>
2026-05-04 15:06:23 +0100

    add piano model and proper lights

--- src/adapters/threejs/ThreeJSRenderer.ts
+++ src/adapters/threejs/ThreeJSRenderer.ts
@@ -1,31 +1,63 @@
 import * as THREE from "three";
 import type { Sizes } from "../../ports/sizes";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {
+  GLTFLoader,
+  type GLTF,
+} from "three/examples/jsm/loaders/GLTFLoader.js";
+import { debugDirectionalLight, debugRenderer } from "./debug";
 
 export class ThreeJSRenderer {
   private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
   private camera: THREE.PerspectiveCamera;
   private orbitControls: OrbitControls;
+  private pianoGltf: GLTF | undefined = undefined;
+  private gltfLoader: GLTFLoader;
 
   constructor(canvas: HTMLCanvasElement, sizes: Sizes) {
     this.renderer = new THREE.WebGLRenderer({
       canvas,
+      antialias: sizes.pixelRatio > 1 ? true : false,
     });
 
     this.renderer.setSize(sizes.width, sizes.height);
     this.renderer.setPixelRatio(sizes.pixelRatio);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFShadowMap;
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 0.8;
 
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio, 0.1, 100);
-    this.camera.position.set(4, 5, 4);
+    this.camera.position.set(0, 4, 10);
     this.scene.add(this.camera);
 
     this.orbitControls = new OrbitControls(this.camera, canvas);
     this.orbitControls.enableDamping = true;
 
+    this.gltfLoader = new GLTFLoader();
+
+    debugRenderer(this.renderer);
+
     this.addTestObjects();
+    this.loadPiano();
+  }
+
+  private async loadPiano() {
+    this.pianoGltf = await this.gltfLoader.loadAsync("/models/toy-piano.glb");
+
+    this.pianoGltf.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+
+    this.pianoGltf.scene.rotation.y = -Math.PI / 2;
+
+    this.scene.add(this.pianoGltf.scene);
   }
 
   private addTestObjects() {
@@ -37,17 +69,24 @@ export class ThreeJSRenderer {
     );
     floorMesh.rotation.x = -Math.PI / 2;
 
-    const boxMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({
-        color: 0x002233,
-      }),
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.far = 15;
+    directionalLight.shadow.mapSize.set(1024, 1024);
+    directionalLight.shadow.normalBias = 0.002;
+    directionalLight.shadow.bias = -0.004;
+    directionalLight.position.set(-5, 5, 8);
+
+    debugDirectionalLight(directionalLight);
+
+    this.scene.add(
+      floorMesh,
+      ambientLight,
+      directionalLight,
+      directionalLight.target,
     );
-    boxMesh.position.y = 1;
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 10);
-
-    this.scene.add(floorMesh, boxMesh, ambientLight);
   }
 
   public updateSizes({ width, height, pixelRatio, aspectRatio }: Sizes) {
---
+++ src/adapters/threejs/debug.ts
@@ -0,0 +1,41 @@
+import GUI from "lil-gui";
+import * as THREE from "three";
+
+const gui = new GUI();
+gui.hide();
+if (document.location.hash.includes("#debug")) {
+  gui.show();
+}
+
+export const debugRenderer = (renderer: THREE.WebGLRenderer) => {
+  const rendererFolder = gui.addFolder("Renderer");
+
+  rendererFolder.add(renderer, "toneMapping", {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+  });
+
+  rendererFolder
+    .add(renderer, "toneMappingExposure")
+    .min(0)
+    .max(10)
+    .step(0.001);
+};
+
+export const debugDirectionalLight = (dirLight: THREE.DirectionalLight) => {
+  const dirLightFolder = gui.addFolder("Directional Light");
+
+  dirLightFolder.add(dirLight.position, "x").min(-20).max(20).step(0.001);
+  dirLightFolder.add(dirLight.position, "y").min(-20).max(20).step(0.001);
+  dirLightFolder.add(dirLight.position, "z").min(-20).max(20).step(0.001);
+  dirLightFolder.add(dirLight, "intensity").min(0).max(20).step(0.001);
+  dirLightFolder
+    .add(dirLight.shadow, "normalBias")
+    .min(-0.05)
+    .max(0.05)
+    .step(0.001);
+  dirLightFolder.add(dirLight.shadow, "bias").min(-0.05).max(0.05).step(0.001);
+};
